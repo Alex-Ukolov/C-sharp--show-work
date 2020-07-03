c117352b2895f77b708f5bf1c462132da6bd70df
алексей Уколов <aleksej.ukolov.01@list.ru>
2020-07-03 13:41:45 +0300

    all

---
+++ 1practicheskaya/App.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Data;
+
+
+namespace _1practicheskaya
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private ICollectionView _dataGridCollection;
+        private string _filterString;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataGridCollection = CollectionViewSource.GetDefaultView(TestData);
+            DataGridCollection.Filter = new Predicate<object>(Filter);
+            
+        }
+
+        public ICollectionView DataGridCollection
+        {
+            get { return _dataGridCollection; }
+            set { _dataGridCollection = value; NotifyPropertyChanged("DataGridCollection"); }
+        }
+
+        public string FilterString
+        {
+            get { return _filterString; }
+            set
+            {
+                _filterString = value;
+                NotifyPropertyChanged("FilterString");
+                FilterCollection();
+            }
+        }
+
+        private void FilterCollection()
+        {
+            if (_dataGridCollection != null)
+            {
+                _dataGridCollection.Refresh();
+            }
+        }
+
+        public bool Filter(object obj)
+        {
+            var data = obj as TestClass;
+            if (data != null)
+            {
+                if (!string.IsNullOrEmpty(_filterString))
+                {
+                    return data.product.Contains(_filterString);
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public IEnumerable<TestClass> TestData
+        {
+            get
+            {
+                yield return new TestClass { product = "11", name = "milk" ,cost= "30 ",uniq="1" };
+                yield return new TestClass { product = "12", name = "bananas", cost = "40", uniq = "1" };
+                yield return new TestClass { product = "13", name = "meat horse", cost = "50", uniq = "1" };
+                yield return new TestClass { product = "14", name = "meat spoke" , cost = "60", uniq = "1" };
+                yield return new TestClass { product = "15", name = "fish", cost = "70", uniq = "1" };
+                yield return new TestClass { product = "16", name = "orange", cost = "80", uniq = "1" };
+                yield return new TestClass { product = "17", name = "potatoes", cost = "90", uniq = "1" };
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+
+
+    public class TestClass
+    {
+        public string product { get; set; }
+        public string name { get; set; }
+        public string cost { get; set; }
+        public string uniq { get; set; }
+    }
+}
---
+++ 1practicheskaya/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Data;
+
+namespace _1practicheskaya
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = TestClass.getTestClass();
+            TestClass st = rrr.SelectedItem as TestClass;
+            string TestClasscost = Convert.ToString(st.cost);
+            int b = Convert.ToInt32(st.cost);
+            int c = 0;
+            c +=b;
+            lex.Text +=""+ c+",";
+        }
+
+        public class TestClass
+        {
+            
+            public string product { get; set; }
+            public string name { get; set; }
+            public string cost { get; set; }
+            public string uniq { get; set; }
+
+            internal static IEnumerable<object> getTestClass()
+            {
+                yield return new TestClass { product = "11", name = "milk", cost = "30 ", uniq = "1" };
+                yield return new TestClass { product = "12", name = "bananas", cost = "40", uniq = "1" };
+                yield return new TestClass { product = "13", name = "meat horse", cost = "50", uniq = "1" };
+                yield return new TestClass { product = "14", name = "meat spoke", cost = "60", uniq = "1" };
+                yield return new TestClass { product = "15", name = "fish", cost = "70", uniq = "1" };
+                yield return new TestClass { product = "16", name = "orange", cost = "80", uniq = "1" };
+                yield return new TestClass { product = "17", name = "potatoes", cost = "90", uniq = "1" };
+            }
+        }
+
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            rrr.ItemsSource = new List<TestClass>(new[] { new TestClass() { product = QQQ.Text, name = WWW.Text, cost = RRR.Text, uniq =TTT.Text } });
+
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            string[] s = lex.Text.Split(',');
+            int n, sum = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                int.TryParse(s[i], out n);
+                sum += n;
+            }
+            lexa.Text = "" + sum;
+        }
+
+        private void Button_Click55(object sender, RoutedEventArgs e)
+        {
+            rrr.ItemsSource =  null;
+        }
+    }
+}
+
